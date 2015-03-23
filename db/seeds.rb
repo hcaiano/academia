d25541b557700bc901e98f0a8d6f24cd745d9cc0
Tiago Martins <tiagom87@gmailc.om>
2015-03-23 11:00:42 +0000

    Added seed questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,220 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 1
+p "Question 1 Created"
+Quiz.create(
+	question: "Assign the variable x to the",
+	answer_1: "x == 5",
+	answer_2: "5 == x",
+	answer_3: "x = 5",
+	answer_4: "5 = x",
+	correct: 3,
+	question_2: "value of 5"
+	)
+# 2
+p "Question 2 Created"
+Quiz.create(
+	question: "What does this expression evaluate to?",
+	answer_1: "YOLO",
+	answer_2: '"YO" + "LO"',
+	answer_3: "YOLO",
+	answer_4: "YO + LO",
+	correct: 1,
+	question_2: '"YO" + "LO"'
+	)
+# 3
+p "Question 3 Created"
+Quiz.create(
+	question: "What is the datatype of",
+	answer_1: "String",
+	answer_2: "Word",
+	answer_3: "Integer",
+	answer_4: "Float",
+	correct: 1,
+	question_2: '"Matt"'
+	)
+# 4
+p "Question 4 Created"
+Quiz.create(
+	question: "What is the datatype of",
+	answer_1: "String",
+	answer_2: "Number",
+	answer_3: "Integer",
+	answer_4: "Float",
+	correct: 2,
+	question_2: "5"
+	)
+# 5
+p "Question 5 Created"
+Quiz.create(
+	question: "What does this expression evaluate to?",
+	answer_1: "bob42",
+	answer_2: '"bob"42',
+	answer_3: '"bob" + 42',
+	answer_4: "error",
+	correct: 4,
+	question_2: '"bob" + 42'
+	)
+# 6
+p "Question 6 Created"
+Quiz.create(
+	question: "What does the following expression evaluate to?",
+	answer_1: "34",
+	answer_2: "7",
+	answer_3: "3 + 4",
+	answer_4: "3,4",
+	correct: 2,
+	question_2: "3 + 4"
+	)
+# 7
+p "Question 7 Created"
+Quiz.create(
+	question: "What does the following expression evaluate to?",
+	answer_1: "1.5",
+	answer_2: "1,5",
+	answer_3: "error",
+	answer_4: "1",
+	correct: 4,
+	question_2: "3/2"
+	)
+# 8
+p "Question 8 Created"
+Quiz.create(
+	question: "What does the following expression evaluate to?",
+	answer_1: "Lisbon",
+	answer_2: "l + i + s + b + o + n",
+	answer_3: "LisboN",
+	answer_4: "LISBON",
+	correct: 4,
+	question_2: '"lisbon".upcase'
+	)
+# 9
+p "Question 9 Created"
+Quiz.create(
+	question: "What is the problem with the following code?",
+	answer_1: "You should not have a comma inside the double quotes",
+	answer_2: "The equal sign should be ==",
+	answer_3: "The order should be reversed",
+	answer_4: "The order should be reversed	You should not have blank spaces when declaring a variable",
+	correct: 4,
+	question_2: 'my variable = "I,m cool"'
+	)
+# 10
+p "Question 10 Created"
+Quiz.create(
+	question: "Make the code run well",
+	answer_1: 'band == "Blink" + 182',
+	answer_2: '"Blink" + 182 == band',
+	answer_3: 'band = "Blink" + 182.to_s',
+	answer_4: 'band == "Blink" + 182.to_s',
+	correct: 3,
+	question_2: 'band = "Blink" + 182'
+	)
+# 11
+p "Question 11 Created"
+Quiz.create(
+	question: 'Convert every letter of "YoLo BrAh" to lowercase.',
+	answer_1: "nice.each do |word| word.down end",
+	answer_2: "nice.each do |word| word.downcase end",
+	answer_3: "nice.downcase()",
+	answer_4: "nice.Downcase",
+	correct: 3,
+	question_2: 'nice = "YoLo BrAh"'
+	)
+# 12
+p "Question 12 Created"
+Quiz.create(
+	question: "Create an array with",
+	answer_1: "{}",
+	answer_2: "[]",
+	answer_3: "()",
+	answer_4: '||',
+	correct: 2,
+	question_2: "nothing inside it"
+	)
+# 13
+p "Question 13 Created"
+Quiz.create(
+	question: "Create an Array that contains the values",
+	answer_1: '{"mike tyson", 89, 9.7}',
+	answer_2: '["mike tyson", 89, 9.7]',
+	answer_3: "[mike tyson, 89, 9.7]",
+	answer_4: "(mike tyson, 89, 9.7)",
+	correct: 2,
+	question_2: '"mike tyson", 89, and 9.7.'
+	)
+# 14
+p "Question 14 Created"
+Quiz.create(
+	question: "What does the following expression evaluate to?",
+	answer_1: "yes",
+	answer_2: "no",
+	answer_3: "true",
+	answer_4: "false",
+	correct: 4,
+	question_2: "4 != 4"
+	)
+# 15
+p "Question 15 Created"
+Quiz.create(
+	question: "What does the following expression evaluate to?",
+	answer_1: "yes",
+	answer_2: "no",
+	answer_3: "true",
+	answer_4: "false",
+	correct: 3,
+	question_2: "5 > 4"
+	)
+# 16
+p "Question 16 Created"
+Quiz.create(
+	question: "What is the value of x when x = 5 and",
+	answer_1: "6",
+	answer_2: "5",
+	answer_3: "5+1",
+	answer_4: "5x",
+	correct: 1,
+	question_2: "x = x + 1"
+	)
+# 17
+p "Question 17 Created"
+Quiz.create(
+	question: "What does the following expression return:",
+	answer_1: '["uno,dos,tres"]',
+	answer_2: '"uno,dos,tres"',
+	answer_3: '"uno","dos","tres"',
+	answer_4: "3",
+	correct: 4,
+	question_2: '["uno", "dos", "tres"].length()'
+	)
+# 18
+p "Question 18 Created"
+Quiz.create(
+	question: "Get the first element of the lyric Array.",
+	answer_1: "lyric.start",
+	answer_2: "lyric{1}",
+	answer_3: "lyric[0]",
+	answer_4: "lyric[1]",
+	correct: 3,
+	question_2: 'lyric = ["laughter", "its", "free"]'
+	)
+# 19
+p "Question 19 Created"
+Quiz.create(
+	question: "Get the last element of the lyric Array.",
+	answer_1: "lyric.pop",
+	answer_2: "lyric[2]",
+	answer_3: "lyric[3]",
+	answer_4: "lyric{3}",
+	correct: 2,
+	question_2: 'lyric = ["laughter", "its", "free"]'
+	)
+# 20
+p "Question 20 Created"
+Quiz.create(
+	question: "How do you print out",
+	answer_1: "Print Hello + World",
+	answer_2: 'Print "Hello + World"',
+	answer_3: 'p "Hello World"',
+	answer_4: 'say "Hello World"',
+	correct: 3,
+	question_2: '"Hello World"'
+	)
